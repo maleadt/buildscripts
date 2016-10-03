8f8e57f18e196c4476ce0154010517917a980782
Tim Besard <tim.besard@gmail.com>
2016-10-03 08:53:34 -0400

    Only replace python path on pre-3.9

--- llvm.sh
+++ llvm.sh
@@ -216,10 +216,10 @@ EOD
 
     local FLAGS=()
 
-    # Fix up references to python if our system python is python3
-    if [[ $(readlink $(which python)) =~ python3 ]]; then
+    # Fix up references to python if our system python is python3 (fixed on 3.9)
+    if [[ $(readlink $(which python)) =~ python3 ]] && verlt "$VERSION" "3.9"; then
         # TODO: is modifying scripts really necessary?
-        ag -l '#!/usr/bin.*python\b' "${SRC_LLVM}" \
+        rg -l '#!/usr/bin.*python\b' "${SRC_LLVM}" \
             | xargs perl -p -i -e 's{(#!/usr/bin.*)python\b}{$1python2}'
         FLAGS+=(-DPYTHON_EXECUTABLE=$(which python2))
     fi
